3ef15531fa05818372a790c0b2799ea60c737a3b
katakahas <ponth02014@gmail.com>
2024-05-08 14:01:59 +0900

    0508

--- src/gpt_read.py
+++ src/gpt_read.py
@@ -1,22 +1,9 @@
 import openai
 import os
 
-OpenAI_api_key = os.environ["OPENAI_API_KEY"]
+def summarize_paper_in_url(url):
+    OpenAI_api_key = os.environ["OPENAI_API_KEY"]
 
-client = openai.OpenAI(api_key=OpenAI_api_key)
+    client = openai.OpenAI(api_key=OpenAI_api_key)
 
-# GPTによる応答生成
-prompt = "以下の条件の下でおいしい食べ物を教えてください。\n条件1:和食\n条件2:甘い"
-response = client.chat.completions.create(
-                    model = "gpt-3.5-turbo-16k-0613",
-                    messages = [
-                        {"role": "system", "content": "You are a helpful assistant."},
-                        {"role": "user", "content": prompt}
-                    ],
-                    temperature=0
-                )
-
-# 応答の表示
-print(response)
-text = response['choices'][0]['message']['content']
-print(text)
+    return "Result message"
--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ import os
 from re import compile
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from gpt_read import summarize_paper_in_url
 
 app_token = os.environ["SLACK_APP_TOKEN"]
 bot_token = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=bot_token)
 
+# responce to "test" message in text channel
 @app.message(compile("test"))
 def test_function(body):
     main_event = body["event"]
@@ -17,17 +19,22 @@ def test_function(body):
     app.client.chat_postMessage(text="test message", channel=channel)
     print("test command is accepted")
 
+# retrieve url and return summary
 @app.event("message")
 def handle_message_events(body):
     main_event = body["event"]
     text_block = main_event["blocks"]
     text_elements = text_block[0]["elements"][0]["elements"]
+    channel = main_event["channel"]
     
     for element in text_elements:
         if element["type"]=="link":
             url = element["url"]
             print("get url: "+url)
+            
+    app.client.chat_postMessage(text=summarize_paper_in_url(url), channel=channel)
 
+# run app bot
 if __name__ == "__main__":
     handler = SocketModeHandler(app, app_token)
     handler.start()
