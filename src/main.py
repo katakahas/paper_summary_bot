7ba6ccf935c0bfccad707356e76028218c3e81aa
katakahas <ponth02014@gmail.com>
2024-05-08 13:49:32 +0900

    readme

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-import random
 from re import compile
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -17,24 +16,16 @@ def listener(body):
     
     print(f"Text message: {text}")
     app.client.chat_postMessage(text="google pixelを使っています", channel=channel)
-    
-@app.message(compile("何をしよう"))
-def what_should_do(body,say):
-    main_event = body["event"]
-    text = main_event["text"]
-    channel = main_event["channel"]
-    
-    languages = ["C#","PHP","Typescript","OCaml"]
-    language = random.choice(languages)
-    app.client.chat_postMessage(text=language+"学ぶべき。", channel=channel)
-    if language=="OCaml":
-        app.client.chat_postMessage(text="そうでもないかも。", channel=channel)
-    print(f"What I should do?: {language}")
-        
+
 @app.event("message")
 def handle_message_events(body, logger):
     print("invalid message")
     logger.info(body)
+    
+@app.event("message.im")
+def direct_message(body):
+    print(body)
+    print("direct messaged")
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, app_token)
