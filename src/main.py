91d767f1ce23ee4ee41cb5eeefb8cae3148341c9
katakahas <ponth02014@gmail.com>
2024-05-11 13:35:04 +0900

    implement former kuma bot

--- src/main.py
+++ src/main.py
@@ -1,23 +1,29 @@
 import os
 from re import compile
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from gpt_read import summarize_paper_in_url
+
+import semantic_utils
+
+# from gpt_read import summarize_paper_in_url
 
 app_token = os.environ["SLACK_APP_TOKEN"]
 bot_token = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=bot_token)
 
+
 # responce to "test" message in text channel
 @app.message(compile("test"))
 def test_function(body):
     main_event = body["event"]
-    text = main_event["text"]
+    # text = main_event["text"]
     channel = main_event["channel"]
-    
-    app.client.chat_postMessage(text="test message", channel=channel)
+
+    app.client.chat_postMessage(text="test accepted", channel=channel)
     print("test command is accepted")
+
 
 # retrieve url and return summary
 @app.event("message")
@@ -26,13 +32,21 @@ def handle_message_events(body):
     text_block = main_event["blocks"]
     text_elements = text_block[0]["elements"][0]["elements"]
     channel = main_event["channel"]
-    
+
+    # take url
+    url = ""
     for element in text_elements:
-        if element["type"]=="link":
+        if element["type"] == "link":
             url = element["url"]
-            print("get url: "+url)
-            
-    app.client.chat_postMessage(text=summarize_paper_in_url(url), channel=channel)
+            print("got url: " + url)
+
+    if url != "":
+        title, tldr, abstract = semantic_utils.title_tldr_and_abstract(url)
+        message = "タイトル: " + title + "\n"
+        message += "TL;DR: " + tldr + "\n"
+        message += "概要: " + abstract
+        app.client.chat_postMessage(text=message, channel=channel)
+
 
 # run app bot
 if __name__ == "__main__":
